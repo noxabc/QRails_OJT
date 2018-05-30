bb5a4be2c2a9fbe466618065cfa3d52ae5c76b9b
Jan Karlo Gacutara <jankarlogacs@gmail.com>
2018-05-30 20:30:22 +0800

    Added compare pickup_location && drop_location

--- app/controllers/api/v1/transport_fares_controller.rb
+++ app/controllers/api/v1/transport_fares_controller.rb
@@ -1,26 +1,26 @@
 class Api::V1::TransportFaresController < ApplicationController
   before_action :require_login
 
-  def index
+  def show_transport_fare
     transportfares = TransportFare.order('created_at ASC');
     render json: transportfares.reload, status: :ok
   end
 
-  def show
+  def get_transport_fare
     transportfare = TransportFare.find(params[:id])
     render json: transportfare.reload, status: :ok
   end
 
-  def update
-    transportfare = TransportFare.find(params[:id])
-    if transportfare.update_attributes(transportfares_params)
-      render json: transportfare.reload, status: :ok
-    else
-      render json: transportfare.errors, status: :bad_request
-    end
-  end
+  # def update_transport_fare
+  #   transportfare = TransportFare.find(params[:id])
+  #   if transportfare.update_attributes(transportfares_params)
+  #     render json: transportfare.reload, status: :ok
+  #   else
+  #     render json: transportfare.errors, status: :bad_request
+  #   end
+  # end
 
-  def compare
+  def compare_location
     if transportfare = TransportFare.where("pickup_location = ? AND drop_location = ?", params[:pickup_location], params[:drop_location])
       render json: { status: 'Success', data:transportfare }, status: :ok
     else
