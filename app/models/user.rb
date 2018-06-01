f153ed175b090e7d4b2ae78fcba964baffa9ed4d
Jan Karlo Gacutara <jankarlogacs@gmail.com>
2018-06-01 22:51:43 +0800

    Added maximum length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,16 @@
 class User < ApplicationRecord
-  has_many :trip_transaction
+  has_many :trip_transactions
   has_secure_password
   has_secure_token :auth_token
   validates_uniqueness_of :username
 
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :middle_name, presence: true
-  validates :username, presence: true
+  validates :last_name, presence: true, length: { maximum: 15 }
+  validates :first_name, presence: true, length: { maximum: 15 }
+  validates :middle_name, presence: true, length: { maximum: 15 }
+  validates :username, presence: true, length: { maximum: 15 }
   validates :password, presence: true
   validates :password_confirmation, presence: true
-  validates :mobile_number, presence: true
+  validates :mobile_number, presence: true, length: { maximum: 11 }
 
   def invalidate_token
     self.update_columns(auth_token: nil)
