3437edec8691c15ac9ff02252fdbcdb3ed78e5d7
Jan Karlo Gacutara <jankarlogacs@gmail.com>
2018-06-01 22:56:12 +0800

    Added compare_location function

--- app/controllers/api/v1/transport_fares_controller.rb
+++ app/controllers/api/v1/transport_fares_controller.rb
@@ -11,14 +11,14 @@ class Api::V1::TransportFaresController < ApplicationController
     render json: transportfare.reload, status: :ok
   end
 
-  # def update_transport_fare
-  #   transportfare = TransportFare.find(params[:id])
-  #   if transportfare.update_attributes(transportfares_params)
-  #     render json: transportfare.reload, status: :ok
-  #   else
-  #     render json: transportfare.errors, status: :bad_request
-  #   end
-  # end
+  def update_transport_fare
+    transportfare = TransportFare.find(params[:id])
+    if transportfare.update_attributes(transportfares_params)
+      render json: transportfare.reload, status: :ok
+    else
+      render json: transportfare.errors, status: :bad_request
+    end
+  end
 
   def compare_location
     if transportfare = TransportFare.where("pickup_location = ? AND drop_location = ?", params[:pickup_location], params[:drop_location]).first
