ea45b38d07ac1d6c967f9b020a011b458377f387
Jan Karlo Gacutara <jankarlogacs@gmail.com>
2018-06-02 21:01:07 +0800

    first commit

--- app/controllers/api/v1/transport_fares_controller.rb
+++ app/controllers/api/v1/transport_fares_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::TransportFaresController < ApplicationController
-  before_action :require_login
+  # before_action :require_login
 
   def show_transport_fare
     transportfares = TransportFare.order('created_at ASC');
